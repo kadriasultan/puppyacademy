5d2bfe718ca3a4099bd53e2e84d1657554796ef3
Kadria Sultan <94522122+kadriasultan@users.noreply.github.com>
2025-05-10 19:34:28 +0200

    js aanpassen

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,7 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.body.classList.add('noscroll');   // scroll blokkeren
+document.body.classList.remove('noscroll'); // scroll toestaan
+
