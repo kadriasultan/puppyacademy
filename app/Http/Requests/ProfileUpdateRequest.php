793a88f028e91cf2f6de46a3aea31019de5232c1
Kadria Sultan <94522122+kadriasultan@users.noreply.github.com>
2025-05-10 12:09:38 +0200

    ProfielUpdateRequest aangemaakt om gegevens te kunnen wijzigen of een hond toe te voegen/verwijderen. Er moet uiteindelijk minimaal één hond aanwezig zijn.

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -1,30 +1,22 @@
 <?php
-
 namespace App\Http\Requests;
 
-use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'lowercase',
-                'email',
-                'max:255',
-                Rule::unique(User::class)->ignore($this->user()->id),
-            ],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($this->user()->id)],
+            'dogs' => ['sometimes', 'array'],
+            'dogs.*.id' => ['sometimes', 'exists:dogs,id'],
+            'dogs.*.name' => ['required', 'string', 'max:255'],
+            'dogs.*.nickname' => ['required', 'string', 'max:255'],
+            'dogs.*.breed' => ['required', 'string', 'max:255'],  // Validatie voor ras
+            'dogs.*.age' => ['required', 'integer', 'min:0'],
         ];
     }
 }
